0a1f664f264e120b705bb612df4c5e6fb7decff0
Contextualist <harzjc@gmail.com>
2021-05-30 23:16:47 -0400

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="shx",
-    version="0.2.0",
+    version="0.3.0",
     author="Contextualist",
     description="For writing async script with Python",
     long_description=long_description,
